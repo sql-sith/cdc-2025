ac41f9f98b5df25c2e936f30967f476d30ca9fe8
Tristan Ribble <71410111+TmRibble@users.noreply.github.com>
2025-01-08 16:31:15 -0600

    Update domain_join.sh

--- src/domain_join.sh
+++ src/domain_join.sh
@@ -72,7 +72,7 @@ if [ $num_controller == "2" ]; then
     read -p "Enter fqdn of domain controller 2 [dc2.example.com]: " domain_controller_2
 
     krb5_conf="[libdefaults]\n\tdefault_realm = ${upper_domain_name}\n\trdns = false\n\tudp_preference_limit = 0\n\n[realms]\n\t${upper_domain_name} = {\n\t\tkdc = ${domain_controller_1}\n\t\tkdc = ${domain_controller_2}\n\t\tmaster_kdc = ${domain_controller_1}\n\t\tadmin_server = ${domain_controller_2}\n\t\tdefault_domain = ${domain_name}\n\t}"
-    sssd_conf="[sssd]\ndomains = ${domain_name}\nconfig_file_version = 2\nservices = nss, pam\ndebug_level = 6\n\n[domain/${domain_name}]\ndefault_shell = /bin/bash\ncache_credentials = False\nfallback_homedir = /home/%d/%u\nuse_fully_qualified_domain_names = True\nrealmd_tags = manages-system joined-with-adcli\n\nad_server = ${domain_controller_1},${domain_controller_2}\nad_hostname=${hostname}.${domain_name}\nad_domain=${domain_name}\n\nldap_uri=ldap://${domain_controller_1}:389,ldap://${domain_controller_2}:389\nldap_backup_uri=ldap://${domain_controller_1}:389,ldap://${domain_controller_2}:389\nldap_id_mapping = True\n\nkrb5_server = ${domain_controller_1},${domain_controller_2}\nkrb5_realm = ${upper_domain_name}\nkrb5_store_password_if_offline = False\n\nid_provider = ad\naccess_provider = ad\nauth_provider = krb5\nchpass_provider = krb5\nsudo_provider = ad\n"
+    sssd_conf="[sssd]\ndomains = ${domain_name}\nconfig_file_version = 2\nservices = nss, pam, sudo\ndebug_level = 6\n\n[domain/${domain_name}]\ndefault_shell = /bin/bash\ncache_credentials = False\nfallback_homedir = /home/%d/%u\nuse_fully_qualified_domain_names = True\nrealmd_tags = manages-system joined-with-adcli\n\nad_server = ${domain_controller_1},${domain_controller_2}\nad_hostname=${hostname}.${domain_name}\nad_domain=${domain_name}\n\nldap_uri=ldap://${domain_controller_1}:389,ldap://${domain_controller_2}:389\nldap_backup_uri=ldap://${domain_controller_1}:389,ldap://${domain_controller_2}:389\nldap_id_mapping = True\n\nkrb5_server = ${domain_controller_1},${domain_controller_2}\nkrb5_realm = ${upper_domain_name}\nkrb5_store_password_if_offline = False\n\nid_provider = ad\naccess_provider = ad\nauth_provider = krb5\nchpass_provider = krb5\nsudo_provider = ad\n"
 
     echo "" ; echo "Configuring krb5.conf and sssd.conf..."
 
@@ -80,7 +80,7 @@ if [ $num_controller == "2" ]; then
     echo -e $sssd_conf > /etc/sssd/sssd.conf
 elif [ -n $num_controller ] || [ "1" -eq $num_controller ]; then
     krb5_conf="[libdefaults]\n\tdefault_realm = ${upper_domain_name}\n\trdns = false\n\tudp_preference_limit = 0\n\n[realms]\n\t${upper_domain_name} = {\n\t\tkdc = ${domain_controller_1}\n\t\tmaster_kdc = ${domain_controller_1}\n\t\tadmin_server = ${domain_controller_1}\n\t\tdefault_domain = ${domain_name}\n\t}"
-    sssd_conf="[sssd]\ndomains = ${domain_name}\nconfig_file_version = 2\nservices = nss, pam\ndebug_level = 6\n\n[domain/${domain_name}]\ndefault_shell = /bin/bash\ncache_credentials = False\nfallback_homedir = /home/%d/%u\nuse_fully_qualified_domain_names = True\nrealmd_tags = manages-system joined-with-adcli\n\nad_server = ${domain_controller_1}\nad_hostname=${hostname}.${domain_name}\nad_domain=${domain_name}\n\nldap_uri=ldap://${domain_controller_1}:389\nldap_backup_uri=ldap://${domain_controller_1}:389\nldap_id_mapping = True\n\nkrb5_server = ${domain_controller_1}\nkrb5_realm = ${upper_domain_name}\nkrb5_store_password_if_offline = False\n\nid_provider = ad\naccess_provider = ad\nauth_provider = krb5\nchpass_provider = krb5\nsudo_provider = ad\n"
+    sssd_conf="[sssd]\ndomains = ${domain_name}\nconfig_file_version = 2\nservices = nss, pam, sudo\ndebug_level = 6\n\n[domain/${domain_name}]\ndefault_shell = /bin/bash\ncache_credentials = False\nfallback_homedir = /home/%d/%u\nuse_fully_qualified_domain_names = True\nrealmd_tags = manages-system joined-with-adcli\n\nad_server = ${domain_controller_1}\nad_hostname=${hostname}.${domain_name}\nad_domain=${domain_name}\n\nldap_uri=ldap://${domain_controller_1}:389\nldap_backup_uri=ldap://${domain_controller_1}:389\nldap_id_mapping = True\n\nkrb5_server = ${domain_controller_1}\nkrb5_realm = ${upper_domain_name}\nkrb5_store_password_if_offline = False\n\nid_provider = ad\naccess_provider = ad\nauth_provider = krb5\nchpass_provider = krb5\nsudo_provider = ad\n"
 
     echo "" ; echo "Configuring krb5.conf and sssd.conf..."
 
@@ -94,30 +94,23 @@ echo "Editing pam configuration..." ; echo ""
 (cat /etc/pam.d/common-session ; echo -e "session required\tpam_mkhomedir.so skel=/etc/skel umask=0022") > /etc/pam.d/common-session
 
 # Sudo
-read -p "Enable sudo control via SSSD/Active Directory [y/n]: " yes_or_no
+read -p "Enter name of group to control sudo access [domain admins]:" sudoers_group_name
 
-if [ $yes_or_no == "y" ] || [ $yes_or_no == "Y" ]; then
-    read -p "Enter name of group to control sudo access [domain admins]:" sudoers_group_name
-
-    if [ -n $sudoers_group_name ]; then
-        sudoers_group_name="domain admins"
-    fi
-
-    sudoers_group_name="${sudoers_group_name// /\\ }"
-    echo "Installing..."
-
-    echo "" | apt-get install -y libsss-sudo 1> /dev/null
-
-    echo "Editing sudoers..."
-
-    chmod u+w,g+w /etc/sudoers
-    (cat /etc/sudoers ; echo -e "%${sudoers_group_name}@${domain_name} ALL=(ALL) ALL") > /etc/sudoers
-    chmod u-w,g-w /etc/sudoers
-
-    echo "Editing sssd.conf..."
-
-    sed "s/services = nss, pam/services = nss, pam, sudo/" /etc/sssd/sssd.conf > /etc/sssd/sssd.conf
+if [ -n $sudoers_group_name ]; then
+    sudoers_group_name="domain admins"
 fi
+
+sudoers_group_name="${sudoers_group_name// /\\ }"
+echo "Installing..."
+
+echo "" | apt-get install -y libsss-sudo 1> /dev/null
+
+echo "Editing sudoers..."
+
+chmod u+w,g+w /etc/sudoers
+(cat /etc/sudoers ; echo -e "%${sudoers_group_name}@${domain_name} ALL=(ALL) ALL") > /etc/sudoers
+chmod u-w,g-w /etc/sudoers
+
 
 # SSH
 echo "" ; read -p "Enable active directory access over ssh [y/n]: " yes_or_no
