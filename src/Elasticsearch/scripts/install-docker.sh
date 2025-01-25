ed0657a1a7c461a6057960c2ef4544fe6c5c2b92
Chris Leonard <chris@databaseguy.com>
2025-01-25 11:41:19 -0600

    Fixed bad shebang lines

--- src/Elasticsearch/scripts/install-docker.sh
+++ src/Elasticsearch/scripts/install-docker.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 # Add Docker's official GPG key:
 sudo apt-get update
--- src/Elasticsearch/scripts/remove-docker.sh
+++ src/Elasticsearch/scripts/remove-docker.sh
@@ -1,4 +1,4 @@
-#!/bash/bin
+#!/bin/bash
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
     sudo apt-get remove $pkg; 
